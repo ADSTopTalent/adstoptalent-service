bb5219b1ffe1ca919bd991f0fb23d546098b04d5
sanjeev sharma <sanjeev.sharma@affluencesolutions.com>
2018-08-07 13:50:18 +0530

    AppUpdated

---
+++ MyFirstSpringBootApp/src/main/java/com/myapp/MyFirstSpringBootApp/Login.java
@@ -0,0 +1,31 @@
+package com.myapp.MyFirstSpringBootApp;
+
+public class Login {
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean validate(String username, String password) {
+		if (username == "admin" && password == "admin") {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+}
