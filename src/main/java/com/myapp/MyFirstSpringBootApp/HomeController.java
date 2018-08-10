da3490fb8089c822ec5eb3e66fbee7287670f1dc
Sanjeev sharma <40255512+Sanjeevsharmaads@users.noreply.github.com>
2018-08-10 09:06:08 +0530

    Update HomeController.java

--- src/main/java/com/myapp/MyFirstSpringBootApp/HomeController.java
+++ src/main/java/com/myapp/MyFirstSpringBootApp/HomeController.java
@@ -6,24 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
 	@RequestMapping("/home")
 	public String welcome() {
 		return "hello world";
 	}
-	
-	
-	
-	
 	@RequestMapping("/products")
 	public @ResponseBody Product getProducts() {
 		Product newProduct = new Product();
 		newProduct.setName("iPhone");
 		newProduct.setDescription("iPhone X Plus Silver");
 		newProduct.setPrice("$1000");
-		return newProduct;
-		
-		
+		return newProduct;	
 	}
 	
 	@RequestMapping("/login")
@@ -34,14 +27,12 @@ public class HomeController {
 
 		if(login.getUsername()=="admin" && login.getPassword()=="admin")
 		{
-			return login;
-			
+			return login;	
 		}
 		else
 		{
 		System.out.println("wrong credentials");
 		}
-		return login;
-		
+		return login;	
 	}
 }
