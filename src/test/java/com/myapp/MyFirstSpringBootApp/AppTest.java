da9e284097a78f27a053227692bc9bc1a76e5161
Sanjeev sharma <40255512+Sanjeevsharmaads@users.noreply.github.com>
2018-08-10 09:10:15 +0530

    Update AppTest.java

--- src/test/java/com/myapp/MyFirstSpringBootApp/AppTest.java
+++ src/test/java/com/myapp/MyFirstSpringBootApp/AppTest.java
@@ -7,9 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase	{
 	//Voting voting=new Voting();
 	Product product=new Product();
 	Login login=new Login();
@@ -18,38 +16,30 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
+    public AppTest( String testName ){
         super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( AppTest.class );
     }
-
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue( true );
     }
-    
- 
+	
 	public void testApp2() {
 		boolean canIVote = product.canVote(25);
 		assertTrue(canIVote);
 	}
-	
 
 	public void testApp3() {
 		boolean validate = login.validate("admin","admin");
 		assertTrue(validate);
 	}
-	
-	
 }
